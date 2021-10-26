e4ab787d4f1bd6588e8b6de16b3823b6eac64872
Webber Lin <webber949@gmail.com>
2021-10-26 23:50:36 +0800

    .Net 5 Google Drive API

---
+++ MyDriveStream/Controllers/DriveApiController.cs
@@ -0,0 +1,63 @@
+﻿using Google.Apis.Drive.v3;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Auth.OAuth2;
+using System.IO;
+using System.Threading;
+using Google.Apis.Util.Store;
+using Google.Apis.Services;
+
+namespace MyDriveStream.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DriveApiController : ControllerBase
+    {
+        private static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        private static string ApplicationName = "MyDriveStream";
+
+        [HttpGet]
+        public IEnumerable<Google.Apis.Drive.v3.Data.File> GetFiles()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.FromStream(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute().Files;
+            if (files != null && files.Count > 0)
+            {
+                return files;
+            }
+            else return new List<Google.Apis.Drive.v3.Data.File>();
+        }
+    }
+}
